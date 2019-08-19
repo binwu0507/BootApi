692b8a4fa6b865c36863338decc1f6f7bb0b4253
Bin Wu <binwu0507@gmail.com>
2019-08-19 18:49:27 -0500

    Try to commit

---
+++ BootApi/Controllers/CustomerController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BootApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BootApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : Controller
+    {
+
+
+        private readonly MyContext _context;
+
+        public CustomerController(MyContext context)
+        {
+            _context = context;
+
+            if(_context.Customers.Count()==0)
+            {
+                _context.Customers.Add(new Customer {  Id = 1, Name = "Bin Wu"});
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomerById(long id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if(customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCustomerById), new { id = customer.Id }, customer);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Customer>> PutCustomrer(int id, Customer customrer)
+        {
+            if(id != customrer.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customrer).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+
+            return NoContent();
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ BootApi/Models/MyContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BootApi.Models
+{
+    public class MyContext: DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
